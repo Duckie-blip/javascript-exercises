6e7ca447ca3071f038a6f34aac80eb7215e66c88
Lukáš DD <easylit@email.cz>
2026-03-29 01:58:09 +0100

    palindromes 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const clean = str.toLowerCase().replace(/[^a-z0-9]/g, "")
+    const rev = clean.split("").reverse().join("");
 
-};
+    return clean === rev
+
+}
 
 // Do not edit below this line
 module.exports = palindromes;
