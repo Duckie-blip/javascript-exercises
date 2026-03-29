66d54364514dcfdab4defb07c8529fc79f1f0578
Lukáš DD <easylit@email.cz>
2026-03-29 01:01:29 +0100

    calculator 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
+const add = function(a, b) {
+	return a + b
+};
+
+const subtract = function(a, b) {
+  return a - b
 	
 };
 
-const subtract = function() {
+const sum = function(arr) {
+  let sum = 0
+
+  if (arr.length === 0){
+    return 0
+  }
+  else {
+    for (let i = 0; i < arr.length; i++){
+
+      sum += arr[i]
+  }
+  }
+	return sum
+};
+
+const multiply = function(arr) {
+  res = arr.reduce((a, b) => a*b, 1)
+  return res
+  };
+
+const power = function(a, b) {
+  return a ** b 
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
